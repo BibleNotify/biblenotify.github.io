ab351571bb97e0ab06f4b2ba474b35099e3614b6
Noah Rahm <correctsyntax@yahoo.com>
2022-01-26 12:27:46 -0600

    New website design

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {  
+  content: ["./*.{html,js}"],  
+  theme: {       
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },  
+    extend: {
+      colors: {            
+        accent: '#7DD273',
+        lightgray: '#EBF0F0',
+        darkgray: '#242424',
+        darkergray: '#1C1C1C'  
+      } 
+    },  
+  },  
+  plugins: [],
+}
