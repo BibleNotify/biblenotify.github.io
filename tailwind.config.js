1cecce0d55f0b50e20171b3e9ce9074372c2c090
Noah Rahm <correctsyntax@yahoo.com>
2026-05-03 13:56:39 -0500

    Update website for v5.x of the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   content: ["./*.{html,js}"],  
   theme: {       
     fontFamily: {
-      sans: ['Roboto', 'sans-serif'],
+      sans: ['Merriweather Sans', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
     },  
     extend: {
       colors: {            
